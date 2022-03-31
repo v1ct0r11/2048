d988bdce24c6c250500509c15b0ae4f82f090ec0
v1ct0r11 <victor.n.bychenkov@gmail.com>
2022-03-31 19:55:38 +0300

    hng

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform mainPanel;
+    public GameObject nodePrefab;
+    public List<Node> nodes;
+    public Block blockPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodes = new List<Node>();
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject node = Instantiate(nodePrefab, mainPanel);
+            nodes.Add(node.GetComponent<Node>());
+        }
+        SpawnBlocks(2);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+ 
+    
+    }
+
+
+    public void SpawnBlocks(int amount=1)
+    {
+        List<Node> freeNodes=nodes.Where(n=>n.occupiedBlock==null).OrderBy(b=>Random.value).ToList();
+        foreach (var node in freeNodes.Take(amount))
+        {
+            var block=Instantiate(blockPrefab, node.pos, Quaternion.identity);
+            node.occupiedBlock = block;
+            block.transform.parent = mainPanel;
+            block.transform.position = node.pos;
+        }
+
+    }
+
+
+
+
+}
+
+
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Vector2 pos=>transform.position;
+    public Block occupiedBlock;
+
+
+
+
+}
